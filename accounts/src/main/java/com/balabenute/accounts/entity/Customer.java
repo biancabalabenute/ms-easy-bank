3f20dca7bb48252761b82a84d725e01bf04ca762
Bianca Balabenute <balabenute.bianca@gmail.com>
2024-08-07 13:10:41 -0300

    criando entity de accounts

---
+++ accounts/src/main/java/com/balabenute/accounts/entity/Customer.java
@@ -0,0 +1,22 @@
+package com.balabenute.accounts.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Customer extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
+    private Long customerId;
+    private String name;
+    private String email;
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+}
