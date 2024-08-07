684333307a0d93d45d816fb2029e8fcf1a388c17
Bianca Balabenute <balabenute.bianca@gmail.com>
2024-08-07 17:00:05 -0300

    CREATE API accounts

--- accounts/src/main/java/com/balabenute/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/balabenute/accounts/controller/AccountsController.java
@@ -1,13 +1,30 @@
 package com.balabenute.accounts.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.balabenute.accounts.constants.AccountsConstants;
+import com.balabenute.accounts.dto.CustomerDto;
+import com.balabenute.accounts.dto.ResponseDto;
+import com.balabenute.accounts.service.IAccountsService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
+@RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class AccountsController {
 
-    @GetMapping("sayHello")
-    public String sayHello(){
-        return "Hi World";
+    private IAccountsService iAccountsService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+        iAccountsService.createAccount(customerDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 }
---
+++ accounts/src/main/java/com/balabenute/accounts/service/impl/AccountServiceImpl.java
@@ -0,0 +1,52 @@
+package com.balabenute.accounts.service.impl;
+
+import com.balabenute.accounts.constants.AccountsConstants;
+import com.balabenute.accounts.dto.CustomerDto;
+import com.balabenute.accounts.entity.Accounts;
+import com.balabenute.accounts.entity.Customer;
+import com.balabenute.accounts.exception.CustomerAlreadyExistsException;
+import com.balabenute.accounts.mapper.CustomerMapper;
+import com.balabenute.accounts.repository.AccountsRepository;
+import com.balabenute.accounts.repository.CustomerRepository;
+import com.balabenute.accounts.service.IAccountsService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+@AllArgsConstructor
+public class AccountServiceImpl implements IAccountsService {
+
+    private AccountsRepository accountsRepository;
+    private CustomerRepository customerRepository;
+
+    @Override
+    public void createAccount(CustomerDto customerDto) {
+        Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        Optional<Customer> optionalCustomer = customerRepository.findByMobileNumber(customerDto.getMobileNumber());
+        if(optionalCustomer.isPresent()) {
+            throw new CustomerAlreadyExistsException("Customer already registered with given mobileNumber "
+                    +customerDto.getMobileNumber());
+        }
+        customer.setCreatedAt(LocalDateTime.now());
+        customer.setCreatedBy("Anonymous");
+        Customer savedCustomer = customerRepository.save(customer);
+        accountsRepository.save(createNewAccount(savedCustomer));
+    }
+
+    private Accounts createNewAccount(Customer customer) {
+        Accounts newAccount = new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randomAccNumber = 1000000000L + new Random().nextInt(900000000);
+
+        newAccount.setAccountNumber(randomAccNumber);
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+        newAccount.setCreatedAt(LocalDateTime.now());
+        newAccount.setCreatedBy("Anonymous");
+        return newAccount;
+    }
+}
