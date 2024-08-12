751df0801bd1db37411ceedba929fbc1f147e223
Bianca Balabenute <balabenute.bianca@gmail.com>
2024-08-12 15:30:59 -0300

    finalizando account

--- accounts/src/main/java/com/balabenute/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/balabenute/accounts/AccountsApplication.java
@@ -1,15 +1,23 @@
 package com.balabenute.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts microservice REST API Documentation",
+                description = "EasyBank Accounts microservice REST API Documentation",
+                version = "v1"))
 public class AccountsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsApplication.class, args);
+    }
 
 }
