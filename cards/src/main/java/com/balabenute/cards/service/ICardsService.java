28ba314fe16adcb0b5b888020f784b76bbe2a844
Bianca Balabenute <balabenute.bianca@gmail.com>
2024-08-13 15:38:24 -0300

    desenvovendo contexto cards e documentando

---
+++ cards/src/main/java/com/balabenute/cards/service/ICardsService.java
@@ -0,0 +1,15 @@
+package com.balabenute.cards.service;
+
+import com.balabenute.cards.dto.CardsDto;
+
+public interface ICardsService {
+
+    void createCard(String mobileNumber);
+
+    CardsDto fetchCard(String mobileNumber);
+
+    boolean updateCard(CardsDto cardsDto);
+
+    boolean deleteCard(String mobileNumber);
+
+}
