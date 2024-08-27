07c2b5fac2d344b0a179117025220524fa7c8ea8
Bianca Balabenute <balabenute.bianca@gmail.com>
2024-08-27 14:45:43 -0300

    configuracoes basicas

--- accounts/src/main/java/com/balabenute/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/balabenute/accounts/AccountsApplication.java
@@ -1,13 +1,16 @@
 package com.balabenute.accounts;
 
+import com.balabenute.accounts.dto.AccountsContactInfoDto;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
 @SpringBootApplication
 @OpenAPIDefinition(
         info = @Info(
